6408cac56684774dc2ff3b91328bd3d993aa036b
tomhuyquang@gmail.com <tomhuyquang@gmail.com>
2025-10-19 17:57:52 +0700

    CRUD Task

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,14 +8,14 @@ const PORT = process.env.PORT || 5001;
 
 dotenv.config();
 
-connectDB();
-
 app.use(express.json());
 
 app.use("/api/tasks", tasksRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
 
 export default app;
