b19c7b448b51c91c1783dcca512981b15aba01da
tomhuyquang@gmail.com <tomhuyquang@gmail.com>
2025-10-26 10:45:10 +0700

    prepare for deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,10 +23,10 @@ app.use("/api/tasks", tasksRouter);
 
 // Serve static files in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
   });
 }
 
