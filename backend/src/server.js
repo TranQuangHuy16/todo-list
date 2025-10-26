f5dfb1b81603b7cedfd814a50f5ff0d3c205f8a0
tomhuyquang@gmail.com <tomhuyquang@gmail.com>
2025-10-26 10:38:35 +0700

    prepare for deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,19 +3,34 @@ import tasksRouter from "./routes/tasksRouter.js";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const app = express();
-
-const PORT = process.env.PORT || 5001;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
-// middlewares
-app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" }));
+const app = express();
+const PORT = process.env.PORT || 5001;
 
+// Middlewares
+app.use(express.json());
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors({ origin: "http://localhost:5173" }));
+}
 app.use("/api/tasks", tasksRouter);
 
+// Serve static files in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+  });
+}
+
+// Connect to DB and start server
 connectDB().then(() => {
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
